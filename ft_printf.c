526cc85b064b54e205c7908e17d69a82b08c8de1
Hicham Amaarour <hamaarou@e1r2p2.1337.ma>
2022-11-01 19:18:27 +0100

    ss

--- ft_printf.c
+++ ft_printf.c
@@ -6,36 +6,47 @@
 /*   By: hamaarou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 08:46:06 by hamaarou          #+#    #+#             */
-/*   Updated: 2022/11/01 18:56:26 by hamaarou         ###   ########.fr       */
+/*   Updated: 2022/11/01 19:17:14 by hamaarou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdarg.h>
 #include <stdio.h>
+int	prnt_second(va_list args, char k)
+{
+	int	c;
 
+	c = 0;
+	if (k == 'c')
+		c += ft_putchar(va_arg(args, int));
+	else if (k == 's')
+		c += ft_putstr(va_arg(args, char *));
+	else if (k == 'd')
+		c += ft_putnbr(va_arg(args, unsigned long));
+	else if (k == 'i')
+		c += ft_putnbr(va_arg(args, unsigned long));
+	else if (k == 'x')
+		c += ft_putnbr_hex(va_arg(args, unsigned long), 'x');
+	else if (k == 'X')
+		c += ft_putnbr_hex(va_arg(args, unsigned long), 'X');
+
+	return (c);
+}
 
 int	ft_printf(const char *str, ...)
 {
-	va_list	args;       
-	int		i;
-	int		c;
+	va_list	args;
+	int 	i;
+	int 	k;
 
-	c = 0;
 	i = 0;
+	k = 0;
 	va_start(args, str);
 	while (str[i])
 	{
-		if (str[i] == '%' && str[i+1] == 'c')
-			c += ft_putchar(va_arg(args, int));
-		else if (str[i] == '%' && str[i+1] == 's')
-			c += ft_putstr(va_arg(args, char *));
-		else if (str[i] == '%' && str[i+1] == 'd')
-			c += ft_putnbr(va_arg(args, unsigned long));
-		else if (str[i] == '%' && str[i+1] == 'i')
-			c += ft_putnbr(va_arg(args, unsigned long));
-			
+		i++;
 	}
 	va_end(args);
-	return (1);
+	return (k);
 }
